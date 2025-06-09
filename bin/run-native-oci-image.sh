1ac4d729094e6072105ff56c401bbae0c21ea183
Salman Malik <salman.the.malik@gmail.com>
2025-06-09 17:43:29 -0400

    making it easier to demo running the native oci-image

---
+++ bin/run-native-oci-image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run --rm -it \
+  -v ./config.properties:/config/config.properties \
+  -e CONFIG_PATH=/config \
+  -e SPRING_PROFILES_ACTIVE=externaldb \
+  -e DB_HOSTNAME=host.docker.internal \
+  -p 8080:8080 \
+  maliksalman/dockerdemo:native-1.0
